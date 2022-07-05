dfd569ea8d0d4e420abd9d566d095cc40bd93df7
GiorgosTsakonas <giorgos_tsakonas@outlook.com>
2022-07-05 21:57:53 +0300

    basic functions

--- script.js
+++ script.js
@@ -25,3 +25,76 @@ function loadSong(song) {
     audio.src = `music/${song}.mp3`
     cover.src = `images/${song}.jpg`
 }
+
+function playSong(){
+musicContainer.classList.add('play')
+playBtn.querySelector('i.fas').classList.remove('fa-play')
+playBtn.querySelector('i.fas').classList.add('fa-pause')
+$(".fa-pause").css("color", "lightblue");
+audio.play()
+}
+
+function pauseSong(){
+musicContainer.classList.remove('play')
+playBtn.querySelector('i.fas').classList.remove('fa-pause')
+playBtn.querySelector('i.fas').classList.add('fa-play')
+$(".fa-play").css("color", "pink");
+audio.pause()
+}
+
+
+function prevSong(){
+    songIndex --
+    if (songIndex < 0){
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function nextSong(){
+    songIndex ++
+    if (songIndex == songs.length){
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+//event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains("play")
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+
+
+//Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
